c09ae1f502325f25c53fee754a76cdef77a6fe1a
Acuna <acuna.public@gmail.com>
2022-08-11 01:31:46 +0300

    Initial commit

--- mygit.bat
+++ mygit.bat
@@ -47,7 +47,7 @@
 		
 	)
 	
-	for /f "delims=" %%a in ('call "!root_dir!functions.bat" "!root_dir!config.ini" Global') do (
+	for /f "delims=" %%a in ('call "!root_dir!config.ini" Global') do (
 		
 		for /f "tokens=1* delims==" %%b in ("%%a") do (
 			set global_config_%%b=%%c
@@ -455,6 +455,34 @@
 		)
 		
 	) else echo Не найден конфиг для текущего репозитория
+	
+	:readconfig
+	setlocal
+	set file=%~1
+	set area=%~2
+	
+	if [%~3] neq [] set area=%~2 "%~3"
+	
+	set currarea=
+	
+	for /f "usebackq delims=" %%a in ("!file!") do (
+		set ln=%%a
+		if "!ln:~0,1!"=="[" (
+			set currarea=!ln!
+		) else (
+			for /f "tokens=1,2 delims==" %%b in ("!ln!") do (
+				
+				set key=%%b
+				set val=%%c
+				
+				if "[!area!]"=="!currarea!" (
+					echo !key!=!val!
+				)
+			)
+		)
+	)
+	
+	endlocal
 	
 	echo.
 	
